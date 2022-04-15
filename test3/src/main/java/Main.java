e742084aacbef3068646e9c61c46cc1c99c120f3
joro1002 <joro1002@abv.bg>
2022-04-15 14:07:38 +0300

    fix config JPA

--- test3/src/main/java/Main.java
+++ test3/src/main/java/Main.java
@@ -1,14 +1,11 @@
-import jakarta.persistence.spi.PersistenceProvider;
-
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("soft_uni");
         EntityManager entityManager = emf.createEntityManager();
-
         Engine engine = new Engine(entityManager);
         engine.run();
     }
