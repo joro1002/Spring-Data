5a5c88832ba93ae5a09bfd587c2dd2066429293b
joro1002 <joro1002@abv.bg>
2022-04-21 13:50:14 +0300

    CodeFirst

---
+++ CodeFirstHibernate/CodeFirst/src/main/java/test/Person.java
@@ -0,0 +1,23 @@
+package test;
+
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.MappedSuperclass;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+public abstract class Person extends BaseEntity{
+    private String name;
+
+    public Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
