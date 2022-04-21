0b87eb797d1b8b62b92b40a60ec99d48a31effa5
joro1002 <joro1002@abv.bg>
2022-04-21 15:23:20 +0300

    CodeFirst

---
+++ CodeFirstExercise/CodeFirst/src/main/java/universitySystem/Course.java
@@ -0,0 +1,85 @@
+package universitySystem;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "courses")
+public class Course extends BaseEntity{
+    private String name;
+    private String description;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private int credits;
+    private Student student;
+    private Teacher teacher;
+
+    public Course() {
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "teacher_id", referencedColumnName = "id")
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "student_id",
+         referencedColumnName = "id")
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "description")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Column(name = "start_date")
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    @Column(name = "end_date")
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    @Column(name = "credits")
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+}
---
+++ CodeFirstExercise/CodeFirst/src/main/java/universitySystem/Teacher.java
@@ -0,0 +1,37 @@
+package universitySystem;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Entity
+@Table(name = "teachers")
+public class Teacher extends Person{
+    private String email;
+    private BigDecimal salaryPerHour;
+
+    public Teacher() {
+    }
+
+
+    @Column(name = "email")
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Column(name = "salary_per_hour")
+    public BigDecimal getSalaryPerHour() {
+        return salaryPerHour;
+    }
+
+    public void setSalaryPerHour(BigDecimal salaryPerHour) {
+        this.salaryPerHour = salaryPerHour;
+    }
+}
