623c1547497f9c9e4c8e91417ddf2ab096145976
joro1002 <joro1002@abv.bg>
2022-05-27 20:26:17 +0300

    exam

---
+++ RealDealExam/RealDeal_Skeleton/src/main/java/softuni/exam/util/ValidationUtilImpl.java
@@ -0,0 +1,21 @@
+package softuni.exam.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+
+public class ValidationUtilImpl implements ValidationUtil{
+    private final Validator validator;
+
+    public ValidationUtilImpl() {
+        this.validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
+
+    @Override
+    public <E> boolean isValid(E entity) {
+        return this.validator.validate(entity).isEmpty();
+    }
+
+}
